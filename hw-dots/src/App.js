41177cf9780215c2f5780aa31b6dbcd19a4e7311
Andrew Gies <agies@cs.washington.edu>
2020-02-28 00:06:34 -0800

    Staff push of files

---
+++ hw-dots/src/App.js
@@ -0,0 +1,56 @@
+/*
+ * Copyright ©2019 Dan Grossman.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 331 for use solely during Autumn Quarter 2019 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+import React, {Component} from 'react';
+import EdgeList from "./EdgeList";
+import Grid from "./Grid";
+import GridSizePicker from "./GridSizePicker";
+
+// Allows us to write CSS styles inside App.css, any any styles will apply to all components inside <App />
+import "./App.css";
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            gridSize: 4,  // The number of points in the grid
+            edges: ""
+        };
+    }
+
+    updateGridSize = (event) => {
+        // Every event handler with JS can optionally take a single parameter that
+        // is an "event" object - contains information about an event. For mouse clicks,
+        // it'll tell you thinks like what x/y coordinates the click was at. For text
+        // box updates, it'll tell you the new contents of the text box, like we're using
+        // below:
+        this.setState({
+            gridSize: parseInt(event.target.value)
+        });
+    };
+
+    render() {
+        const canvas_size = 500;
+        return (
+            <div>
+                <p id="app-title">Connect the Dots!</p>
+                <GridSizePicker value={this.state.gridSize} onChange={this.updateGridSize}/>
+                <Grid size={this.state.gridSize} width={canvas_size} height={canvas_size}/>
+                <EdgeList value={""} onChange={(event) => {console.log(event.target.value)}}/>
+            </div>
+
+        );
+    }
+
+}
+
+export default App;
---
+++ hw-dots/src/Grid.js
@@ -0,0 +1,104 @@
+/*
+ * Copyright ©2019 Dan Grossman.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 331 for use solely during Autumn Quarter 2019 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+/* A simple grid with a variable size */
+/* Most of the assignment involves changes to this class */
+
+import React, {Component} from 'react';
+
+/**
+ * Props:
+ *
+ * width - the desired width of the grid area
+ * height - the desired height of the grid area
+ * size - the number of points along a single axis in the grid
+ */
+class Grid extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            backgroundImage: null  // An image object to render into the canvas.
+        };
+        this.canvasReference = React.createRef();
+    }
+
+    componentDidMount() {
+        // Since we're saving the image in the state and re-using it any time we
+        // redraw the canvas, we only need to load it once, when our component first mounts.
+        this.fetchAndSaveImage();
+        this.redraw();
+    }
+
+    componentDidUpdate() {
+        this.redraw()
+    }
+
+    fetchAndSaveImage() {
+        // Creates an Image object, and sets a callback function
+        // for when the image is done loading (it might take a while).
+        let background = new Image();
+        background.onload = () => {
+            let newState = {
+                backgroundImage: background
+            };
+            this.setState(newState);
+        };
+        // Once our callback is set up, we tell the image what file it should
+        // load from. This also triggers the loading process.
+        background.src = "./image.jpg";
+    }
+
+    redraw = () => {
+        let ctx = this.canvasReference.current.getContext('2d');
+        ctx.clearRect(0, 0, this.props.width, this.props.height);
+        // Once the image is done loading, it'll be saved inside our state.
+        // Otherwise, we can't draw the image, so skip it.
+        if (this.state.backgroundImage !== null) {
+            ctx.drawImage(this.state.backgroundImage, 0, 0);
+        }
+        // Draw all the dots.
+        let coordinates = this.getCoordinates();
+        for(let coordinate of coordinates) {
+            this.drawCircle(ctx, coordinate);
+        }
+    };
+
+    getCoordinates = () => {
+        // A hardcoded 4x4 grid. Probably not going to work when we change the grid size...
+        return [
+            [100, 100], [100, 200], [100, 300], [100, 400],
+            [200, 100], [200, 200], [200, 300], [200, 400],
+            [300, 100], [300, 200], [300, 300], [300, 400],
+            [400, 100], [400, 200], [400, 300], [400, 400]
+        ];
+    };
+
+    drawCircle = (ctx, coordinate) => {
+        ctx.fillStyle = "white";
+        // Generally use a radius of 4, but when there are lots of dots on the grid (> 50)
+        // we slowly scale the radius down so they'll all fit next to each other.
+        let radius = Math.min(4, 100 / this.props.size);
+        ctx.beginPath();
+        ctx.arc(coordinate[0], coordinate[1], radius, 0, 2 * Math.PI);
+        ctx.fill();
+    };
+
+    render() {
+        return (
+            <div id="grid">
+                <canvas ref={this.canvasReference} width={this.props.width} height={this.props.height}/>
+                <p>Current Grid Size: 4</p>
+            </div>
+        );
+    }
+}
+
+export default Grid;
