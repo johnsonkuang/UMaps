dd9dde1431bb06fb4d37bc69ec2568eeb01a8f43
Andrew Gies <agies@cs.washington.edu>
2020-03-05 20:45:38 -0800

    Staff push of files

---
+++ hw-campuspaths-server/src/main/java/campuspaths/SparkServer.java
@@ -0,0 +1,18 @@
+package campuspaths;
+
+import campuspaths.utils.CORSFilter;
+
+public class SparkServer {
+
+    public static void main(String[] args) {
+        CORSFilter corsFilter = new CORSFilter();
+        corsFilter.apply();
+        // The above two lines help set up some settings that allow the
+        // React application to make requests to the Spark server, even though it
+        // comes from a different server.
+        // You should leave these two lines at the very beginning of main().
+
+        // TODO: Create all the Spark Java routes you need here.
+    }
+
+}
